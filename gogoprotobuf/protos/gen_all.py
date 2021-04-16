119c62453bbe622a4f464a3e10801a42e5cda8e5
MichaelWang <michael_wangguanyi@163.com>
2021-04-16 11:39:46 +0800

    marshaler && size

--- gogoprotobuf/protos/gen_all.py
+++ gogoprotobuf/protos/gen_all.py
@@ -7,7 +7,7 @@ vendor_path = f'{project_path}/vendor'
 proto_file_path = f'{gogoprotobuf_demo_path}/protos'
 
 file_names = ['enum_prefix', 'getters', 'face', 'nullable', 'customname', 'stringer', 'gostring', 'populate',
-              'customtype', 'testgen']
+              'customtype', 'testgen', 'marshaler_size']
 
 index = int(sys.argv[1])
 
